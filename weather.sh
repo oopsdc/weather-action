1676cdf267ab8cfc87c76f1b5eeda4184ddcbd8f
oopsdc <oopsdc@qq.com>
2021-11-06 16:14:34 +0800

    update

--- weather.sh
+++ weather.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl wttr.in/Taiyuan?format="今日天气:+%C\n<br>湿度:+++++%h\n<br>体表温度:+%f\n<br>风向:+++++%w\n<br>日出:+++++%S\n<br>日落:+++++%s\n<br>" >> result.txt
+curl wttr.in/Taiyuan?format="今日天气:\t+%C\n<br>湿度:\t+++++%h\n<br>体表温度:\t+%f\n<br>风向:\t+++++%w\n<br>日出:\t+++++%S\n<br>日落:\t+++++%s\n<br>" >> result.txt
 # curl wttr.in/Taiyuan?format=v2 >> result.txt
