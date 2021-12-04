a2eeb31c3ac5cdeb9660a6f2134769fbef6be156
oopsdc <oopsdc@qq.com>
2021-12-04 16:50:47 +0800

    update

--- weather.sh
+++ weather.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl wttr.in/Taiyuan?format="<ul><li>今日天气:+%C\n<li>湿度:+++++%h\n<li>体表温度:+%f\n<li>风向:+++++%w\n<li>日出:+++++%S\n<li>日落:+++++%s\n</ul>" >> result.txt
+curl wttr.in/Longnan?format="<ul><li>陇南天气:+%C\n<li>湿度:+++++%h\n<li>体表温度:+%f\n<li>风向:+++++%w\n<li>日出:+++++%S\n<li>日落:+++++%s\n</ul>" >> result.txt
